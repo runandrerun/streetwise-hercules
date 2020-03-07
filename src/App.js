76bdb9c49f9c94f65e3306449fa28baca9771e4a
runandrerun <the.asantiagojr@gmail.com>
2020-03-06 22:05:22 -0500

    Completed Hero component. Added mixins and animations. Setup variables. Button component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Hero from './components/hero/Hero.component';
-import './App.css';
+import './App.styles.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Hero />
     </div>
   );
--- src/components/hero/Hero.component.jsx
+++ src/components/hero/Hero.component.jsx
@@ -4,7 +4,17 @@ import './Hero.styles.scss';
 const Hero = () => {
   return (
     <header className="hero">
-
+      <div className="hero__heading-container">
+        <div className="heading-primary">
+          <div className="heading-primary--main">
+            streetwise
+          </div>
+          <div className="heading-primary--sub">
+            fosters to the rescue
+          </div>
+          <a href="#" className="btn btn--white btn--animated">Learn more</a>
+        </div>
+      </div>
     </header>
   );
 };
